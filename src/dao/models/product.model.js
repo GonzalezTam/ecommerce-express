6c1e0d9f885d29abc393f5d57038ae8f0827024a
GonzalezTam <agustingonzaleztam@gmail.com>
2023-07-22 02:59:37 -0300

    Product model fix

--- src/dao/models/product.model.js
+++ src/dao/models/product.model.js
@@ -5,7 +5,7 @@ const productCollection = 'products';
 const productSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true, min: 5000, max: 110000 },
+  price: { type: Number, required: true },
   stock: { type: Number, required: true },
   code: { type: String, required: true, unique: true },
   status: { type: Boolean, required: true },
