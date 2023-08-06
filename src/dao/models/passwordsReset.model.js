b09159ccc8383aa5d5f0f551898f4244a0427c01
GonzalezTam <agustingonzaleztam@gmail.com>
2023-08-06 19:58:53 -0300

    Added index to collection to make expireAfterSeconds work

--- src/dao/models/passwordsReset.model.js
+++ src/dao/models/passwordsReset.model.js
@@ -5,8 +5,10 @@ const passwordsResetSchema = new mongoose.Schema({
   email: { type: String, ref: 'users' },
   token: { type: String, required: true },
   isUsed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now, expireAfterSeconds: 3600 }
+  createdAt: { type: Date, default: Date.now }
 });
+// add index to expire documents after 1 hour
+passwordsResetSchema.index({ createdAt: 1 }, { expireAfterSeconds: 3600 });
 
 const passwordsResetModel = mongoose.model(passwordsResetCollection, passwordsResetSchema);
 
